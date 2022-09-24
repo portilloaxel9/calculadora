6b7026749af50e6d72da66f95d4223208c0bd82b
Axel Emiliano Portillo1 <aportillo1@Lenovo.com>
2022-09-24 15:36:09 -0300

    first commit

---
+++ calculadora.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class calculadora
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(calculadora))
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnRestar = New System.Windows.Forms.Button()
+        Me.btnSumar = New System.Windows.Forms.Button()
+        Me.btnMultiplicar = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnDividir = New System.Windows.Forms.Button()
+        Me.Pantalla = New System.Windows.Forms.TextBox()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnPunto = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn1.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(14, 239)
+        Me.btn1.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(80, 65)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn2.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(104, 239)
+        Me.btn2.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(80, 65)
+        Me.btn2.TabIndex = 1
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn3.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(194, 239)
+        Me.btn3.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(80, 65)
+        Me.btn3.TabIndex = 2
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btnRestar
+        '
+        Me.btnRestar.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnRestar.Font = New System.Drawing.Font("Bauhaus 93", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRestar.Location = New System.Drawing.Point(284, 89)
+        Me.btnRestar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnRestar.Name = "btnRestar"
+        Me.btnRestar.Padding = New System.Windows.Forms.Padding(5, 0, 0, 0)
+        Me.btnRestar.Size = New System.Drawing.Size(80, 65)
+        Me.btnRestar.TabIndex = 3
+        Me.btnRestar.Text = "-"
+        Me.btnRestar.UseVisualStyleBackColor = False
+        '
+        'btnSumar
+        '
+        Me.btnSumar.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnSumar.Font = New System.Drawing.Font("Bauhaus 93", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSumar.Location = New System.Drawing.Point(284, 14)
+        Me.btnSumar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnSumar.Name = "btnSumar"
+        Me.btnSumar.Padding = New System.Windows.Forms.Padding(5, 0, 0, 0)
+        Me.btnSumar.Size = New System.Drawing.Size(80, 65)
+        Me.btnSumar.TabIndex = 4
+        Me.btnSumar.Text = "+"
+        Me.btnSumar.UseVisualStyleBackColor = False
+        '
+        'btnMultiplicar
+        '
+        Me.btnMultiplicar.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnMultiplicar.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiplicar.Location = New System.Drawing.Point(284, 164)
+        Me.btnMultiplicar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnMultiplicar.Name = "btnMultiplicar"
+        Me.btnMultiplicar.Padding = New System.Windows.Forms.Padding(3, 0, 0, 5)
+        Me.btnMultiplicar.Size = New System.Drawing.Size(80, 65)
+        Me.btnMultiplicar.TabIndex = 5
+        Me.btnMultiplicar.Text = "x"
+        Me.btnMultiplicar.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn7.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(14, 89)
+        Me.btn7.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(80, 65)
+        Me.btn7.TabIndex = 10
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn8.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(104, 89)
+        Me.btn8.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(80, 65)
+        Me.btn8.TabIndex = 9
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn6.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(194, 164)
+        Me.btn6.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(80, 65)
+        Me.btn6.TabIndex = 8
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn5.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(104, 164)
+        Me.btn5.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(80, 65)
+        Me.btn5.TabIndex = 7
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn4.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(14, 164)
+        Me.btn4.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(80, 65)
+        Me.btn4.TabIndex = 6
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn9.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(194, 89)
+        Me.btn9.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(80, 65)
+        Me.btn9.TabIndex = 11
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btnDividir
+        '
+        Me.btnDividir.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnDividir.Font = New System.Drawing.Font("Bauhaus 93", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDividir.Location = New System.Drawing.Point(284, 239)
+        Me.btnDividir.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnDividir.Name = "btnDividir"
+        Me.btnDividir.Padding = New System.Windows.Forms.Padding(5, 0, 0, 3)
+        Me.btnDividir.Size = New System.Drawing.Size(80, 65)
+        Me.btnDividir.TabIndex = 12
+        Me.btnDividir.Text = "/"
+        Me.btnDividir.UseVisualStyleBackColor = False
+        '
+        'Pantalla
+        '
+        Me.Pantalla.Enabled = False
+        Me.Pantalla.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Pantalla.Location = New System.Drawing.Point(14, 14)
+        Me.Pantalla.Margin = New System.Windows.Forms.Padding(5)
+        Me.Pantalla.Multiline = True
+        Me.Pantalla.Name = "Pantalla"
+        Me.Pantalla.Size = New System.Drawing.Size(260, 65)
+        Me.Pantalla.TabIndex = 13
+        Me.Pantalla.Text = "0"
+        Me.Pantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.BackColor = System.Drawing.Color.Red
+        Me.btnBorrar.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBorrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnBorrar.Location = New System.Drawing.Point(284, 314)
+        Me.btnBorrar.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Padding = New System.Windows.Forms.Padding(3, 0, 0, 5)
+        Me.btnBorrar.Size = New System.Drawing.Size(80, 65)
+        Me.btnBorrar.TabIndex = 17
+        Me.btnBorrar.Text = "C"
+        Me.btnBorrar.UseVisualStyleBackColor = False
+        '
+        'btnIgual
+        '
+        Me.btnIgual.BackColor = System.Drawing.Color.Green
+        Me.btnIgual.Font = New System.Drawing.Font("Cascadia Mono SemiBold", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIgual.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnIgual.Location = New System.Drawing.Point(194, 314)
+        Me.btnIgual.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Padding = New System.Windows.Forms.Padding(5, 0, 0, 5)
+        Me.btnIgual.Size = New System.Drawing.Size(80, 65)
+        Me.btnIgual.TabIndex = 16
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn0.Font = New System.Drawing.Font("Dubai Light", 20.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.Location = New System.Drawing.Point(14, 314)
+        Me.btn0.Margin = New System.Windows.Forms.Padding(5)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(80, 65)
+        Me.btn0.TabIndex = 15
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnPunto
+        '
+        Me.btnPunto.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnPunto.Font = New System.Drawing.Font("Bauhaus 93", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPunto.Location = New System.Drawing.Point(104, 314)
+        Me.btnPunto.Margin = New System.Windows.Forms.Padding(5)
+        Me.btnPunto.Name = "btnPunto"
+        Me.btnPunto.Padding = New System.Windows.Forms.Padding(5, 0, 0, 10)
+        Me.btnPunto.Size = New System.Drawing.Size(80, 65)
+        Me.btnPunto.TabIndex = 14
+        Me.btnPunto.Text = "."
+        Me.btnPunto.UseVisualStyleBackColor = False
+        '
+        'calculadora
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(379, 395)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnPunto)
+        Me.Controls.Add(Me.Pantalla)
+        Me.Controls.Add(Me.btnDividir)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnMultiplicar)
+        Me.Controls.Add(Me.btnSumar)
+        Me.Controls.Add(Me.btnRestar)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "calculadora"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnRestar As Button
+    Friend WithEvents btnSumar As Button
+    Friend WithEvents btnMultiplicar As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnDividir As Button
+    Friend WithEvents Pantalla As TextBox
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnPunto As Button
+End Class
---
+++ calculadora.vb
@@ -0,0 +1,126 @@
+﻿Public Class calculadora
+    Dim Operacion As String
+    Dim ValorResultado As Nullable(Of Double) = Nothing
+    Dim Valor2 As Nullable(Of Double) = Nothing
+    Dim SePresionaOperador As Boolean
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Concatenar()
+        Pantalla.Text &= "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Concatenar()
+        Pantalla.Text &= "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Concatenar()
+        Pantalla.Text &= "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Concatenar()
+        Pantalla.Text &= "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        Concatenar()
+        Pantalla.Text &= "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        Concatenar()
+        Pantalla.Text &= "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        Concatenar()
+        Pantalla.Text &= "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        Concatenar()
+        Pantalla.Text &= "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        Concatenar()
+        Pantalla.Text &= "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        Concatenar()
+        Pantalla.Text &= "0"
+    End Sub
+
+    Private Sub btnPunto_Click(sender As Object, e As EventArgs) Handles btnPunto.Click
+        Concatenar()
+        If InStr(Pantalla.Text, ".", CompareMethod.Text) = 0 Then
+            Pantalla.Text &= "."
+        End If
+    End Sub
+
+    Private Sub btnSumar_Click(sender As Object, e As EventArgs) Handles btnSumar.Click
+        HacerOperacion()
+        Operacion = "+"
+    End Sub
+
+    Private Sub btnRestar_Click(sender As Object, e As EventArgs) Handles btnRestar.Click
+        HacerOperacion()
+        Operacion = "-"
+    End Sub
+
+    Private Sub btnMultiplicar_Click(sender As Object, e As EventArgs) Handles btnMultiplicar.Click
+        HacerOperacion()
+        Operacion = "x"
+    End Sub
+
+    Private Sub btnDividir_Click(sender As Object, e As EventArgs) Handles btnDividir.Click
+        HacerOperacion()
+        Operacion = "/"
+    End Sub
+
+    Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+        HacerOperacion()
+        Operacion = ""
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        Borrar()
+    End Sub
+
+    Public Sub HacerOperacion()
+        SePresionaOperador = True
+        Valor2 = Val(Pantalla.Text)
+        If ValorResultado <> 0 Then
+            Select Case Operacion
+                Case "+"
+                    ValorResultado = ValorResultado + Valor2
+                Case "-"
+                    ValorResultado = ValorResultado - Valor2
+                Case "x"
+                    ValorResultado = ValorResultado * Valor2
+                Case "/"
+                    ValorResultado = ValorResultado / Valor2
+            End Select
+            Pantalla.Text = ValorResultado
+        Else
+            ValorResultado = Valor2
+        End If
+    End Sub
+
+    Public Sub Concatenar()
+        If SePresionaOperador = True Then
+            Pantalla.Text = ""
+            SePresionaOperador = False
+        ElseIf Pantalla.Text = "0" Then
+            Pantalla.Text = ""
+        End If
+    End Sub
+
+    Public Sub Borrar()
+        ValorResultado = Nothing
+        Valor2 = Nothing
+        Pantalla.Text = "0"
+    End Sub
+End Class
